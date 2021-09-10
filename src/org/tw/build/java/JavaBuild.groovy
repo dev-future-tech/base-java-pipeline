8f73fbdda1cbda3f36dbea93c53ab8662d926a81
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-09-10 10:34:38 -0700

    First try

---
+++ src/org/tw/build/java/JavaBuild.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+
+package org.tw.build.java
+
+class WelcomeMessage {
+    static String message = "Welcome to GridCI"
+}
---
+++ vars/javabuild.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def welcome() {
+    echo "Today I want to welcome you. ${WelcomeMessage}"
+}
+
+def call() {
+    welcome()
+}
