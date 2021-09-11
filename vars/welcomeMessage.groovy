366f720b216f70bab9f772d6f487a292d0bf5023
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-09-10 19:18:23 -0700

    reducing scope

--- vars/welcomeMessage.groovy
+++ vars/welcomeMessage.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
-import org.tw.build.java.JavaBuild
 
-def call() {
-    echo "Today I want to welcome you. ${JavaBuild.message}"
+def call(String name = 'human') {
+    echo "Today I want to welcome you."
 }
