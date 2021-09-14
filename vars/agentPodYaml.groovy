25936e355d51b54037af42f131a5ee343291ba93
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-09-13 17:47:27 -0700

    Initialising the YamlBuild class

--- vars/agentPodYaml.groovy
+++ vars/agentPodYaml.groovy
@@ -4,9 +4,10 @@ import org.tw.build.java.YamlBuild
 
 def getWithContainers(String... containers = ['human']) {
     def podYaml = ''
+    def yamlBuild = new YamlBuild()
     containers.each{ containerName -> 
         echo "This container is ${containerName}"
-        podYaml += YamlBuild.getHeader(containerName)
+        podYaml += yamlBuild.getHeader(containerName)
     }
     return podYaml
 }
