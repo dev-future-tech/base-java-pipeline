450dcb46fbe3b6fa0ff473e0d6cd9422ae05033f
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-10-01 15:03:04 -0700

    added def

--- vars/notifyPeople.groovy
+++ vars/notifyPeople.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(subject = "${JOB_NAME} - ${BUILD_NUMBER}", body = "Notifying for job ${JOB_NAME} at build ${BUILD_NUMBER}", recipients='User <no-reply@natonalgrid.com>') {
-    notifier = new org.tw.build.java.Notifier();
+    def notifier = new org.tw.build.java.Notifier();
     notifier.sendNotification([recipient: recipients, subject: subject, body: body])
 }
 
