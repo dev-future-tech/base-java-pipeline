de34009796df70b1580bf083b15e071f1556281e
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-10-01 15:23:20 -0700

    Testing to see if emails are still sending

--- vars/notifyPeople.groovy
+++ vars/notifyPeople.groovy
@@ -2,7 +2,11 @@
 import org.tw.build.java.Notifier
 
 def call(subject = "${JOB_NAME} - ${BUILD_NUMBER}", body = "Notifying for job ${JOB_NAME} at build ${BUILD_NUMBER}", recipients='User <anthony.ikeda@thoughtworks.com>') {
-    def notifier = new Notifier();
-    notifier.sendNotification([recipient: recipients, subject: subject, body: body])
+    // def notifier = new Notifier();
+    // notifier.sendNotification([recipient: recipients, subject: subject, body: body])
+
+    emailext body: body,
+        subject: subject,
+        to: recipient
 }
 
