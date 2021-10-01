c69d2c86b69424f0ce3607b974f01a6c1067180c
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-10-01 13:32:36 -0700

    Trying different way to call the notifyPeople method

--- vars/notifyPeople.groovy
+++ vars/notifyPeople.groovy
@@ -1,10 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String recipient = 'anthony.ikeda@thoughtworks.com', 
-            String subject = 'Welcome to GridCI',
-            String body = 'GridCI is open for business!!!') {
-    emailext body: body,
-        subject: subject,
-        to: recipient
+def call(Map params) {
+    emailext body: params.body,
+        subject: params.subject,
+        to: params.recipient
 }
 
