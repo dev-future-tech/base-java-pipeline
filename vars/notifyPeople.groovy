bd219b2a6707953620f99f0b026b3fea8cd3e6e1
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-10-01 15:09:30 -0700

    Altered the way Notifier is imported

--- vars/notifyPeople.groovy
+++ vars/notifyPeople.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
+import org.tw.build.java.Notifier
 
 def call(subject = "${JOB_NAME} - ${BUILD_NUMBER}", body = "Notifying for job ${JOB_NAME} at build ${BUILD_NUMBER}", recipients='User <no-reply@natonalgrid.com>') {
-    def notifier = new org.tw.build.java.Notifier();
+    def notifier = new Notifier();
     notifier.sendNotification([recipient: recipients, subject: subject, body: body])
 }
 
