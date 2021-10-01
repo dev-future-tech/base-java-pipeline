bed90949501af1deea8796bd4b90758974d41162
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-10-01 13:39:39 -0700

    Setting defaults for the notify params

--- vars/notifyPeople.groovy
+++ vars/notifyPeople.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 def sendNotification(recipient = 'anthony.ikeda@thoughtworks.com', subject = 'The real subject', body = 'Empty default body') {
+    echo "Sending to ${recipient}"
     emailext body: body,
         subject: subject,
         to: recipient
