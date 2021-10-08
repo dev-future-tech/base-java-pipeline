50cd8db57d81caf66aec8bb4bad0c428622c40d9
Anthony Ikeda <anthony.ikeda@gmail.com>
2021-10-08 10:30:51 -0700

    Corrected the import in the dynamicBuild var

--- vars/dynamicBuild.groovy
+++ vars/dynamicBuild.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-import org.tw.build.java.DynamicBuilder
+import org.tw.build.java.DynamicBuild
 import org.tw.build.model.ContainerConfig
 
 def call() {
-    def builder = new DynamicBuilder()
+    def builder = new DynamicBuild()
 
     def java = new ContainerConfig(name: 'java', image: 'openjdk:11-oracle', command: 'sleep', args: '99d')
 
